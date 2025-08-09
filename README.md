2f35f0f509eafa6ac1874696ba4bbe6ca763c810
Kratik01 <teamanonymous002@gmail.com>
2025-08-09 21:42:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# LUCIVPS
+Welcome to DRM bot made by @chitijrajput (Instagram)
+
+Note: CP DRM SUPPORTED MINIMUM QUALITY 360
+
+Direct Deploy via click these button 
+
+## Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.heroku.com/deploy?template=https://github.com/CHITIJRAJPUTX/CHITIJSTUBCLONE/tree/main)
---
+++ vars.py
@@ -0,0 +1,10 @@
+
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "28712726")
+API_HASH  = os.environ.get("API_HASH", "06acfd441f9c3402ccdb1945e8e2a93b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8466143564:AAHrulKJRCeC5C6dfaszBrz5CXDez-hjzjs")
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
